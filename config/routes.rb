fc3c3aca15be6687f640a53c3099ad39fd8a2567
kendralockard <kendra.lockard@teecom.com>
2020-05-26 17:53:56 -0700

    hmph

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "recipes/new", to: "recipes#new"
 
-  resources :recipes, only: :create
+  resources :recipes, only: [:create, :new]
 end
